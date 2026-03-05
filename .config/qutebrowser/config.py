883db713380e5e71c7e150d069198861e7b4a8dd
1ces0ul <67786490+1ces0ul@users.noreply.github.com>
2026-03-05 16:51:18 +0800

    Update config.py

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -270,8 +270,10 @@ c.completion.height = '30%'
 c.completion.scrollbar.width = 12
 # 补全类别设置
 c.completion.open_categories = ['filesystem']  # 打开补全时显示文件系统类别
+
 # 提示模式设置
-c.hints.chars = 'asdfghjkl'
+c.hints.delay = 0
+c.hints.chars = 'asdfghjkl;'
 c.hints.mode = 'letter'
 c.hints.auto_follow = 'always'
 c.hints.auto_follow_timeout = 0
